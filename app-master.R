3e0fdab9f06f2a561a470d8c203631a0e54af4aa
harris-mier20 <harris.mier20@imperial.ac.uk>
2023-11-04 18:44:36 +0000

    added regions on map for ui
    regions still need titles and new colours and changes in opacity when hovering

--- app-master.R
+++ app-master.R
@@ -2,15 +2,19 @@ library(shiny)
 library(leaflet)
 library(shinydashboard)
 library(fresh)
+library(sf)
 
-# Define the import statement for Hammersmith One bold
+#load in the file that defines all the postcode regions
+source("create-map-regions.R")
+
+# Define the import statement for Hammersmith One bold font
 font_import_statement <- "https://fonts.googleapis.com/css?family=Hammersmith+One"
 
-# Create a custom theme
+# Create a custom theme for the app using the fresh library
 my_theme <- create_theme(
   adminlte_color(light_blue = "black"),
   adminlte_global(content_bg = "white"),
-  adminlte_sidebar(width = "400px"),
+  adminlte_sidebar(width = "35%"),
   custom_css = sprintf(
     ".main-header .logo { font-family: 'Hammersmith One', sans-serif; font-weight: 400; font-size: 24px; }",
     font_import_statement
@@ -25,39 +29,43 @@ dbHeader <- dashboardHeader(
   
   #define the title 
   title = span("Bike Hire Demand Forecast", style = title_style)
-  
-  #include the logo
-  #title = tags$img(src = "logo.png")
 )
 
 # Define the UI
 ui <- dashboardPage(
   
+  #load in the header that is defined above
   dbHeader,
   
-  #Sidebar styling
+  #Sidebar formatting and outputs
   dashboardSidebar(
-    textOutput("clickMessage")
+    textOutput("PostcodeTitle") 
   ),
   
   #Main body styling
   dashboardBody(
+    
+    #load in the defined themes
     use_theme(my_theme),
+    
+    #fill the screen with the map
     div(
       style = "position: relative;",
       leafletOutput("mymap", height = "100vh"),
+      
+    #overlay the logo  
     div(
         imageOutput("image"),
-        style = "position: absolute; top: 10px; left: 50px;")
+        style = "position: absolute; top: 20px; left: 60px;")
     )
   )
 )
 
 server <- function(input, output, session) {
   
-  #render the logo
+  #render the logo in the overlay imageOutput() element
   output$image <- renderImage({
-    list(src = "logo.png", contentType = "image/png", width = "250px")
+    list(src = "logo.png", contentType = "image/png", width = "350px", deleteFile=FALSE)
   })
   
   # Initialize click message for troubleshooting
@@ -65,29 +73,47 @@ server <- function(input, output, session) {
   
   # Define the map output using the renderLeaflet() function
   output$mymap <- renderLeaflet({
-    # Create the map using leaflet() and specify the map details
+    
+    # Create the map using leaflet()
     leaflet() %>%
-      setView(lng = -0.130, lat = 51.5194, zoom = 12) %>%
+      
+      #define where the map is zoomed by default
+      setView(lng = -0.110, lat = 51.5200, zoom = 14) %>%
       addProviderTiles(providers$Stadia.StamenTonerLite, options = providerTileOptions(noWrap = TRUE)) %>%
-      addCircleMarkers(lng = -0.130, lat = 51.5194, radius = 50, layerId = "circle1") %>%
-      addCircleMarkers(lng = -0.140, lat = 51.5294, radius = 50, layerId = "circle2")
+      
+      #add postcode regions to the map
+      addPolygons(data = wc1_sf, fillColor = "red", fillOpacity = 0.5, layerId = "wc1", options = pathOptions(clickable = TRUE)) %>%
+      addPolygons(data = wc2_sf, fillColor = "red", fillOpacity = 0.5, layerId = "wc2", options = pathOptions(clickable = TRUE)) %>%
+      addPolygons(data = ec1_sf, fillColor = "red", fillOpacity = 0.5, layerId = "ec1", options = pathOptions(clickable = TRUE)) %>%
+      addPolygons(data = ec2_sf, fillColor = "red", fillOpacity = 0.5, layerId = "ec2", options = pathOptions(clickable = TRUE)) %>%
+      addPolygons(data = ec3_sf, fillColor = "red", fillOpacity = 0.5, layerId = "ec3", options = pathOptions(clickable = TRUE)) %>%
+      addPolygons(data = ec4_sf, fillColor = "red", fillOpacity = 0.5, layerId = "ec4", options = pathOptions(clickable = TRUE))
+  
   })
   
-  # Listen for when the map is clicked
-  observeEvent(input$mymap_marker_click, {
-    # Find what element has been clicked by using its ID
-    clicked_marker <- input$mymap_marker_click$id
+  #wait for a shape to be clicked
+  observeEvent(input$mymap_shape_click, {
+    postcode <- input$mymap_shape_click$id
     
-    # Logic to determine output action depending on what element is clicked
-    if (clicked_marker == "circle1") {
-      click_message("Circle 1 is clicked")
-    } else if (clicked_marker == "circle2") {
-      click_message("Circle 2 is clicked")
+    #logic to handle actions based on what postcode is clicked
+    if (postcode == "wc1") {
+      click_message("WC1")
+    } else if (postcode == "wc2") {
+      click_message("WC2")
+    } else if (postcode == "ec1") {
+      click_message("EC1")
+    } else if (postcode == "ec2") {
+      click_message("EC2")
+    } else if (postcode == "ec3") {
+      click_message("EC3")
+    } else if (postcode == "ec4") {
+      click_message("EC4")
     }
+      
   })
   
   # Render the troubleshooting message in the message output
-  output$clickMessage <- renderText({
+  output$PostcodeTitle <- renderText({
     click_message()
   })
 }
