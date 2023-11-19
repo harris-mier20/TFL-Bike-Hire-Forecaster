60c8f5d0acd05ea5d5bb1626cbd7a5d7fe11c8f8
harris-mier20 <harris.mier20@imperial.ac.uk>
2023-11-19 16:46:45 +0000

    prepared file for forecasting

--- predictive-data-compilation.R
+++ predictive-data-compilation.R
@@ -21,17 +21,39 @@ library(openair)
 dailyactivity <- read.csv("data/daily-activity-by-postcode.csv")
 weatherdata <- read.csv("data/weather-data.csv")
 
+#function to smooth data
+smooth_data <- function(data,alpha,starting_value){
+  
+  #define parameters
+  aa <- alpha
+  l0 <- starting_value
+  
+  #initiate vector to store smoothed data
+  smoothed_data <- numeric(length(data))
+  
+  #loop through the data and perform exponential smoothing
+  for (i in 1:length(smoothed_data)) {
+    smoothed_data[i] <- aa * data[i] + (1 - aa) * l0
+    l0 <- smoothed_data[i]
+  }
+  return(smoothed_data)
+}
+
+#loop through all the postcodes in the defined training data and smooth each column
+#with exponential smoothing
+for (i in 3:8){
+  dailyactivity[[i]] <- smooth_data(dailyactivity[[i]],0.1,1250)
+}
+
+#training data starts from 2020-06-01, test data ends on 2022-07-05,
+#split out data after june 2020 to start training
 dailyactivity.train.begin <- which(dailyactivity$Date=="2020-06-01")
-
 dailyactivity.train.end <- which(dailyactivity$Date=="2022-07-05")
-
 dailyactivity.train<- dailyactivity[dailyactivity.train.begin:dailyactivity.train.end,]
 
-#training data starts from 2020-06-01, test data ends on 2022-07-05, forecast dates end on 2024-12-31
-#from the weather data, remove all the rows that have dates on the 29th and beyond in each month
-#to fit with the training and test data
+#weather forecasts end on 2024-12-31 from the weather data, remove all the rows that have
+#dates on the 29th and beyond in each month to fit with the training and test data
 weather_train_data <- weatherdata[1:765,]
-
 weather_train_data.1_28 <- selectByDate(weather_train_data, day = seq(1,28,1))
 
 #creating a empty template dataframe
@@ -48,50 +70,51 @@ Location.features$Wind <- weather_train_data.1_28$windspeed
 
 # function which phase shifts an input data by amount of days infills blank space
 #with mean of entire dataset.
-dataimputaion <- function(inputdata,days){
+dataimputation <- function(inputdata,days){
   ls <- inputdata
   imputationdata <- rep(list(mean(inputdata)),days)
   ls <- append(ls, imputationdata, after = 0)
-  
-  
   ls <- ls[dailyactivity.train.begin:dailyactivity.train.end]
   
   return(ls)
 }
 
-# function which phase shifts an input data by amount of days infills blank space
-#with mean of entire dataset, as its phase shifting it fills the new dataset 
-#with the mean values of the priot "days" number of days#
-meaninputs <- function(inputdata,days){
-  output <- ls()
-  i <- days
-  ls <- inputdata
-  output <- rep(list(mean(inputdata)),days)
-  
-  for (i in days:nrow(Location.features.WC1)) {
-    output[i] <- mean(ls[c((i-days):i)])
+#function that takes an index and a number of days
+#returns the mean of that number of days before that index in the data set
+find_mean <- function(index,data,days){
+  values_to_check = numeric()
+  for (i in 1:days){
+    values_to_check <- c(values_to_check,data[index-i])
   }
-  print(output)
-  output <- output[dailyactivity.train.begin:dailyactivity.train.end]
-  return(output)
+  return(mean(values_to_check))
+}
+
+#function that takes a postcode number of days and returns an array that is the mean
+#for those days previous days for each data point in the training data for that postcode
+mean_array <- function(data, days){
+  means <- numeric()
+  for (i in 1:length(data)){
+    means <- c(means, find_mean((dailyactivity.train.begin-1+i),data,days))
+  }
+  means <- means[1:length(dailyactivity.train$EC1)]
 }
 
 ###creating wc1###
 Location.features.WC1 <- Location.features
 #inserts daily demand of the area and the day of the week
-Location.features.WC1$Daydemand <- dailyactivity.train$WC1[dailyactivity.train.begin:dailyactivity.train.end]
+Location.features.WC1$Daydemand <- dailyactivity.train$WC1
 Location.features.WC1$dayofweek <- dailyactivity.train$Weekday
 #inserts data for linking the current days demand with the day 7,30 and 365 days
 #ago's demand
-Location.features.WC1$Demand1dayago <- dataimputaion(dailyactivity.train$WC1,1)
-Location.features.WC1$Demand7daysago <- dataimputaion(dailyactivity.train$WC1,7)
-Location.features.WC1$Demand30daysago <- dataimputaion(dailyactivity.train$WC1,28)
-Location.features.WC1$Demand365daysago <- dataimputaion(dailyactivity.train$WC1,365)
+Location.features.WC1$Demand1dayago <- dataimputation(dailyactivity$WC1,1)
+Location.features.WC1$Demand7daysago <- dataimputation(dailyactivity$WC1,7)
+Location.features.WC1$Demand30daysago <- dataimputation(dailyactivity$WC1,28)
+Location.features.WC1$Demand365daysago <- dataimputation(dailyactivity$WC1,365)
 #inserts data for linking the current days demand with the past 7,30 and 365 days
 #means
-Location.features.WC1$Meanpast7days <- meaninputs(dailyactivity.train$WC1,7)
-Location.features.WC1$Meanpast30days <- meaninputs(dailyactivity.train$WC1,28)
-Location.features.WC1$Meanpast365days <- meaninputs(dailyactivity.train$WC1,365)
+Location.features.WC1$Meanpast7days <- mean_array(dailyactivity$WC1,7)
+Location.features.WC1$Meanpast30days <- mean_array(dailyactivity$WC1,28)
+Location.features.WC1$Meanpast365days <- mean_array(dailyactivity$WC1,365)
 
 #outputs the dat toa csv file
 Location.features.WC1 <- apply(Location.features.WC1,2,as.character)
@@ -100,19 +123,19 @@ write.csv(Location.features.WC1, "data/feature-data/WC1-feature-data.CSV", row.n
 ###creating WC2###
 Location.features.WC2 <- Location.features
 #inserts daily demand of the area and the day of the week
-Location.features.WC2$Daydemand <- dailyactivity.train$WC2[dailyactivity.train.begin:dailyactivity.train.end]
+Location.features.WC2$Daydemand <- dailyactivity.train$WC2
 Location.features.WC2$dayofweek <- dailyactivity.train$Weekday
 #inserts data for linking the current days demand with the day 7,30 and 365 days
 #ago's demand
-Location.features.WC2$Demand1dayago <- dataimputaion(dailyactivity.train$WC2,1)
-Location.features.WC2$Demand7daysago <- dataimputaion(dailyactivity.train$WC2,7)
-Location.features.WC2$Demand30daysago <- dataimputaion(dailyactivity.train$WC2,30)
-Location.features.WC2$Demand365daysago <- dataimputaion(dailyactivity.train$WC2,365)
+Location.features.WC2$Demand1dayago <- dataimputation(dailyactivity$WC2,1)
+Location.features.WC2$Demand7daysago <- dataimputation(dailyactivity$WC2,7)
+Location.features.WC2$Demand30daysago <- dataimputation(dailyactivity$WC2,30)
+Location.features.WC2$Demand365daysago <- dataimputation(dailyactivity$WC2,365)
 #inserts data for linking the current days demand with the past 7,30 and 365 days
 #means
-Location.features.WC2$Meanpast7days <- meaninputs(dailyactivity.train$WC2,7)
-Location.features.WC2$Meanpast30days <- meaninputs(dailyactivity.train$WC2,30)
-Location.features.WC2$Meanpast365days <- meaninputs(dailyactivity.train$WC2,365)
+Location.features.WC2$Meanpast7days <- mean_array(dailyactivity$WC2,7)
+Location.features.WC2$Meanpast30days <- mean_array(dailyactivity$WC2,30)
+Location.features.WC2$Meanpast365days <- mean_array(dailyactivity$WC2,365)
 
 #outputs the dat toa csv file
 Location.features.WC2 <- apply(Location.features.WC2,2,as.character)
@@ -121,19 +144,19 @@ write.csv(Location.features.WC2, "data/feature-data/WC2-feature-data.CSV", row.n
 ###creating EC1###
 Location.features.EC1 <- Location.features
 #inserts daily demand of the area and the day of the week
-Location.features.EC1$Daydemand <- dailyactivity.train$EC1[dailyactivity.train.begin:dailyactivity.train.end]
+Location.features.EC1$Daydemand <- dailyactivity.train$EC1
 Location.features.EC1$dayofweek <- dailyactivity.train$Weekday
 #inserts data for linking the current days demand with the day 7,30 and 365 days
 #ago's demand
-Location.features.EC1$Demand1dayago <- dataimputaion(dailyactivity.train$EC1,1)
-Location.features.EC1$Demand7daysago <- dataimputaion(dailyactivity.train$EC1,7)
-Location.features.EC1$Demand30daysago <- dataimputaion(dailyactivity.train$EC1,30)
-Location.features.EC1$Demand365daysago <- dataimputaion(dailyactivity.train$EC1,365)
+Location.features.EC1$Demand1dayago <- dataimputation(dailyactivity$EC1,1)
+Location.features.EC1$Demand7daysago <- dataimputation(dailyactivity$EC1,7)
+Location.features.EC1$Demand30daysago <- dataimputation(dailyactivity$EC1,30)
+Location.features.EC1$Demand365daysago <- dataimputation(dailyactivity$EC1,365)
 #inserts data for linking the current days demand with the past 7,30 and 365 days
 #means
-Location.features.EC1$Meanpast7days <- meaninputs(dailyactivity.train$EC1,7)
-Location.features.EC1$Meanpast30days <- meaninputs(dailyactivity.train$EC1,30)
-Location.features.EC1$Meanpast365days <- meaninputs(dailyactivity.train$EC1,365)
+Location.features.EC1$Meanpast7days <- mean_array(dailyactivity$EC1,7)
+Location.features.EC1$Meanpast30days <- mean_array(dailyactivity$EC1,30)
+Location.features.EC1$Meanpast365days <- mean_array(dailyactivity$EC1,365)
 
 #outputs the dat toa csv file
 Location.features.EC1 <- apply(Location.features.EC1,2,as.character)
@@ -142,19 +165,19 @@ write.csv(Location.features.EC1, "data/feature-data/EC1-feature-data.CSV", row.n
 ###creating EC2###
 Location.features.EC2 <- Location.features
 #inserts daily demand of the area and the day of the week
-Location.features.EC2$Daydemand <- dailyactivity.train$EC2[dailyactivity.train.begin:dailyactivity.train.end]
+Location.features.EC2$Daydemand <- dailyactivity.train$EC2
 Location.features.EC2$dayofweek <- dailyactivity.train$Weekday
 #inserts data for linking the current days demand with the day 7,30 and 365 days
 #ago's demand
-Location.features.EC2$Demand1dayago <- dataimputaion(dailyactivity.train$EC2,1)
-Location.features.EC2$Demand7daysago <- dataimputaion(dailyactivity.train$EC2,7)
-Location.features.EC2$Demand30daysago <- dataimputaion(dailyactivity.train$EC2,30)
-Location.features.EC2$Demand365daysago <- dataimputaion(dailyactivity.train$EC2,365)
+Location.features.EC2$Demand1dayago <- dataimputation(dailyactivity$EC2,1)
+Location.features.EC2$Demand7daysago <- dataimputation(dailyactivity$EC2,7)
+Location.features.EC2$Demand30daysago <- dataimputation(dailyactivity$EC2,30)
+Location.features.EC2$Demand365daysago <- dataimputation(dailyactivity$EC2,365)
 #inserts data for linking the current days demand with the past 7,30 and 365 days
 #means
-Location.features.EC2$Meanpast7days <- meaninputs(dailyactivity.train$EC2,7)
-Location.features.EC2$Meanpast30days <- meaninputs(dailyactivity.train$EC2,30)
-Location.features.EC2$Meanpast365days <- meaninputs(dailyactivity.train$EC2,365)
+Location.features.EC2$Meanpast7days <- mean_array(dailyactivity$EC2,7)
+Location.features.EC2$Meanpast30days <- mean_array(dailyactivity$EC2,30)
+Location.features.EC2$Meanpast365days <- mean_array(dailyactivity$EC2,365)
 
 #outputs the dat toa csv file
 Location.features.EC2 <- apply(Location.features.EC2,2,as.character)
@@ -163,19 +186,19 @@ write.csv(Location.features.EC2, "data/feature-data/EC2-feature-data.CSV", row.n
 ###creating EC3###
 Location.features.EC3 <- Location.features
 #inserts daily demand of the area and the day of the week
-Location.features.EC3$Daydemand <- dailyactivity.train$EC3[dailyactivity.train.begin:dailyactivity.train.end]
+Location.features.EC3$Daydemand <- dailyactivity.train$EC3
 Location.features.EC3$dayofweek <- dailyactivity.train$Weekday
 #inserts data for linking the current days demand with the day 7,30 and 365 days
 #ago's demand
-Location.features.EC3$Demand1dayago <- dataimputaion(dailyactivity.train$EC3,1)
-Location.features.EC3$Demand7daysago <- dataimputaion(dailyactivity.train$EC3,7)
-Location.features.EC3$Demand30daysago <- dataimputaion(dailyactivity.train$EC3,30)
-Location.features.EC3$Demand365daysago <- dataimputaion(dailyactivity.train$EC3,365)
+Location.features.EC3$Demand1dayago <- dataimputation(dailyactivity$EC3,1)
+Location.features.EC3$Demand7daysago <- dataimputation(dailyactivity$EC3,7)
+Location.features.EC3$Demand30daysago <- dataimputation(dailyactivity$EC3,30)
+Location.features.EC3$Demand365daysago <- dataimputation(dailyactivity$EC3,365)
 #inserts data for linking the current days demand with the past 7,30 and 365 days
 #means
-Location.features.EC3$Meanpast7days <- meaninputs(dailyactivity.train$EC3,7)
-Location.features.EC3$Meanpast30days <- meaninputs(dailyactivity.train$EC3,30)
-Location.features.EC3$Meanpast365days <- meaninputs(dailyactivity.train$EC3,365)
+Location.features.EC3$Meanpast7days <- mean_array(dailyactivity$EC3,7)
+Location.features.EC3$Meanpast30days <- mean_array(dailyactivity$EC3,30)
+Location.features.EC3$Meanpast365days <- mean_array(dailyactivity$EC3,365)
 
 #outputs the dat toa csv file
 Location.features.EC3 <- apply(Location.features.EC3,2,as.character)
@@ -184,19 +207,19 @@ write.csv(Location.features.EC3, "data/feature-data/EC3-feature-data.CSV", row.n
 ###creating EC4###
 Location.features.EC4 <- Location.features
 
-Location.features.EC4$Daydemand <- dailyactivity.train$EC4[dailyactivity.train.begin:dailyactivity.train.end]
+Location.features.EC4$Daydemand <- dailyactivity.train$EC4
 Location.features.EC4$dayofweek <- dailyactivity.train$Weekday
 #inserts data for linking the current days demand with the day 7,30 and 365 days
 #ago's demand
-Location.features.EC4$Demand1dayago <- dataimputaion(dailyactivity.train$EC4,1)
-Location.features.EC4$Demand7daysago <- dataimputaion(dailyactivity.train$EC4,7)
-Location.features.EC4$Demand30daysago <- dataimputaion(dailyactivity.train$EC4,30)
-Location.features.EC4$Demand365daysago <- dataimputaion(dailyactivity.train$EC4,365)
+Location.features.EC4$Demand1dayago <- dataimputation(dailyactivity$EC4,1)
+Location.features.EC4$Demand7daysago <- dataimputation(dailyactivity$EC4,7)
+Location.features.EC4$Demand30daysago <- dataimputation(dailyactivity$EC4,30)
+Location.features.EC4$Demand365daysago <- dataimputation(dailyactivity$EC4,365)
 #inserts data for linking the current days demand with the past 7,30 and 365 days
 #means
-Location.features.EC4$Meanpast7days <- meaninputs(dailyactivity.train$EC4,7)
-Location.features.EC4$Meanpast30days <- meaninputs(dailyactivity.train$EC4,30)
-Location.features.EC4$Meanpast365days <- meaninputs(dailyactivity.train$EC4,365)
+Location.features.EC4$Meanpast7days <- mean_array(dailyactivity.train$EC4,7)
+Location.features.EC4$Meanpast30days <- mean_array(dailyactivity.train$EC4,30)
+Location.features.EC4$Meanpast365days <- mean_array(dailyactivity.train$EC4,365)
 
 #outputs the dat toa csv file
 Location.features.EC4 <- apply(Location.features.EC4,2,as.character)
